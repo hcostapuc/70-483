16762d93378cc854a90e49b897bb9f913a6839bf
Hebert Costa Andrade <hcostapuc@gmail.com>
2020-03-17 21:02:15 -0300

    diagnostic, logging debug, assets debug, trace

--- LISTING 3-32 Code optimization/LISTING 3-32 Code optimization/Program.cs
+++ LISTING 3-32 Code optimization/LISTING 3-32 Code optimization/Program.cs
@@ -6,6 +6,9 @@ namespace LISTING_3_32_Code_optimization
     {
         static void Main(string[] args)
         {
+            //CTO: se gerarmos o assemblye em modo RELEASE, todas as variaveis não utilizadas e comentarios
+            //são excluidos do assemblie, ja em modo DEBUG, não.
+            //Os breakpoints também são gravados no assemblye em modo DEBUG como "nop" (no operation) já no release não
             int i = 99;
             int j = 100;
             Console.WriteLine("The value in j is {0} ", j);
