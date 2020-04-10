88788c42806b92b818a642a7357f685069df668a
Hebert Costa Andrade <hcostapuc@gmail.com>
2020-04-10 09:34:43 -0300

    hasset generic collection

--- LISTING 4-60 Set example/LISTING 4-60 Set example/Program.cs
+++ LISTING 4-60 Set example/LISTING 4-60 Set example/Program.cs
@@ -7,10 +7,16 @@ namespace LISTING_4_60_Set_example
     {
         static void Main(string[] args)
         {
+            //CTO: o hash set é uma classe generia de lista muito util, para quando:
+            // - a lista não pode conter itens repetidos, o hashset adiciona somente itens unicos, ignorando adição de itens duplicados como o exemplo do "Fast"
+            //   do hasset t1Styles
+            // - comparar uma lista com a outra para identificar se contem itens em comum de ambos
+            // - diferenciar lista, uni-las e testa-las
             HashSet<string> t1Styles = new HashSet<string>();
             t1Styles.Add("Electronic");
             t1Styles.Add("Disco");
-            t1Styles.Add("Fast" );
+            t1Styles.Add("Fast");
+            t1Styles.Add("Fast");
 
             HashSet<string> t2Styles = new HashSet<string>();
             t2Styles.Add("Orchestral");
